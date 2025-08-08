e7bd693b07448673946781dbfed6549933728593
Le Thu Nguyen <nglthu1967@gmail.com>
2025-08-08 16:16:21 +0000

    fix

--- project/complete/src/main/java/com/example/servingwebcontent/Controller/UserController.java
+++ project/complete/src/main/java/com/example/servingwebcontent/Controller/UserController.java
@@ -1,28 +1,22 @@
 package com.example.servingwebcontent.Controller;
 
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.lang.RuntimeException;
 import java.util.ArrayList;
 
-import com.example.servingwebcontent.Component.ListUser;
-import com.example.servingwebcontent.Component.WriteToFile;
-import com.example.servingwebcontent.Database.aivenConnection;
+
 import com.example.servingwebcontent.Database.insertToAiven;
 import com.example.servingwebcontent.Model.User;
 
 @Controller
 public class UserController {
 
-	@GetMapping("/useradd")
+	@GetMapping("/user")
 	public String User(Model model) {
 		model.addAttribute("User", new User());
 
@@ -32,13 +26,13 @@ public class UserController {
 	@PostMapping("/usersave")
 	public String SaveData(Model model, @ModelAttribute User User) {
 		System.out.println("Hello addresss :::" + User);
+		System.out.println("getAddress:::" + User.getAddress());
+		System.out.println("getUsername:::" + User.getUserName());
 
 		model.addAttribute("User", User); // Add populated object back to model for display
 
 		try {
 
-			// model.addAttribute("name", fname);
-			// model.addAttribute("address",address);
 
 			User u = new User();
 			u.setUserName(User.getUserName());
